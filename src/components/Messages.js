0e19eddd6a0bac5a57d8fdc157a7e27d08cc9802
Sangeet Kumar <91060606+skdeveloper8@users.noreply.github.com>
2023-11-17 10:01:15 +0530

    Add files via upload

---
+++ src/components/Messages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Messages() {
+  return (
+    <div className='message'>
+
+    </div>
+  )
+}
+
+export default Messages
---
+++ src/components/Welcome.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Robot from '../assets/robot.gif'
+
+function Welcome({currentUser}) {
+
+  return (
+   <>
+   <div className='welcome'>
+   <img src={Robot} alt="robot" />
+   <h1>
+    Welcome <span>{currentUser.username}!</span>
+   </h1>
+   <h3>Please Select a chat to start messeging</h3>
+   </div>
+   </>
+  )
+}
+
+export default Welcome
